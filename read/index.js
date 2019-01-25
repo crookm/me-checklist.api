e6fdd85a5186b8d07b33de556c094bb66788f02a
Matt Crook <matt.crook98@gmail.com>
2019-01-25 21:12:51 +1300

    cleanup

--- read/index.js
+++ read/index.js
@@ -4,13 +4,15 @@ const client = new mongo(process.env.mongo, { useNewUrlParser: true });
 let db, progress;
 
 exports.read = async (req, res) => {
-  res.header('Content-Type','application/json');
+  res.header("Content-Type", "application/json");
 
-  if (!client.isConnected()) await client.connect().catch(err => console.error(err));
+  if (!client.isConnected())
+    await client.connect().catch(err => console.error(err));
+
   db = db || client.db("checkdata");
   progress = progress || db.collection("progress");
 
-  console.log(req.query("test"));
+  console.log(req.query.test);
 
   res.send(await progress.findOne({ hi: "there" }));
 };
