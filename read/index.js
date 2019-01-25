15aa492a68dbcbe87c4444d1bdbd106997a20d47
Matt Crook <matt.crook98@gmail.com>
2019-01-25 21:03:58 +1300

    fix exports

--- read/index.js
+++ read/index.js
@@ -3,9 +3,9 @@ const client = new mongo(process.env.mongo, { useNewUrlParser: true });
 
 let db, progress;
 
-module.read = async (req, res) => {
+exports.read = async (req, res) => {
   res.header('Content-Type','application/json');
-  
+
   if (!client.isConnected()) await client.connect().catch(err => console.error(err));
   db = db || client.db("checkdata");
   progress = progress || db.collection("progress");
