65f954ffe41d17e35625a293a4ac2226b2ed3fa1
Matt Crook <matt.crook98@gmail.com>
2019-01-26 12:34:14 +1300

    handle cors preflight

--- read/index.js
+++ read/index.js
@@ -8,45 +8,53 @@ const valid_games = ["1", "2", "3"];
 let db, progress;
 
 exports.read = async (req, res) => {
-  res.header("Content-Type", "application/json");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Origin", "*");
 
-  if (!client.isConnected())
-    await client.connect().catch(err => {
-      console.error(err);
-      res.status(500).send({ error: "server" });
-    });
+  if (req.method === "OPTIONS") {
+    // handle CORS preflight
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "600");
+    res.status(204).send("");
+  } else {
+    res.header("Content-Type", "application/json");
+    
+    if (!client.isConnected())
+      await client.connect().catch(err => {
+        console.error(err);
+        res.status(500).send({ error: "server" });
+      });
 
-  // require passphrase in body
-  if (
-    !req.body["passphrase"] ||
-    !req.query["game"] ||
-    !valid_games.includes(req.query["game"])
-  )
-    return res.status(400).send({ error: "client" });
+    // require passphrase in body
+    if (
+      !req.body["passphrase"] ||
+      !req.query["game"] ||
+      !valid_games.includes(req.query["game"])
+    )
+      return res.status(400).send({ error: "client" });
 
-  db = db || client.db("checkdata");
-  progress = progress || db.collection("progress");
+    db = db || client.db("checkdata");
+    progress = progress || db.collection("progress");
 
-  let passphrase = sjcl.codec.hex.fromBits(
-    sjcl.hash.sha256.hash(req.body["passphrase"])
-  );
+    let passphrase = sjcl.codec.hex.fromBits(
+      sjcl.hash.sha256.hash(req.body["passphrase"])
+    );
 
-  let doc = await progress.findOne(
-    { passphrase },
-    { project: { [`data.${req.query["game"]}`]: 1 } }
-  );
+    let doc = await progress.findOne(
+      { passphrase },
+      { project: { [`data.${req.query["game"]}`]: 1 } }
+    );
 
-  if (!doc) return res.status(404).send({ error: "not found" });
+    if (!doc) return res.status(404).send({ error: "not found" });
 
-  res.send(doc["data"][req.query["game"]]); // return the doc
+    res.send(doc["data"][req.query["game"]]); // return the doc
 
-  await progress.updateOne(
-    { _id: doc._id },
-    {
-      $set: { "analytics.read_last": new Date() },
-      $inc: { "analytics.read": 1 }
-    }
-  );
+    await progress.updateOne(
+      { _id: doc._id },
+      {
+        $set: { "analytics.read_last": new Date() },
+        $inc: { "analytics.read": 1 }
+      }
+    );
+  }
 };
--- writemerge/index.js
+++ writemerge/index.js
@@ -8,91 +8,99 @@ const valid_games = ["1", "2", "3"];
 let db, progress, list, listdata;
 
 exports.writemerge = async (req, res) => {
-  res.header("Content-Type", "application/json");
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.set("Access-Control-Allow-Origin", "*");
 
-  if (!client.isConnected())
-    await client.connect().catch(err => {
-      console.error(err);
-      res.status(500).send({ error: "server" });
-    });
+  if (req.method === "OPTIONS") {
+    // handle CORS preflight
+    res.set("Access-Control-Allow-Methods", "GET, POST");
+    res.set("Access-Control-Allow-Headers", "Content-Type");
+    res.set("Access-Control-Max-Age", "600");
+    res.status(204).send("");
+  } else {
+    res.header("Content-Type", "application/json");
+    
+    if (!client.isConnected())
+      await client.connect().catch(err => {
+        console.error(err);
+        res.status(500).send({ error: "server" });
+      });
 
-  // require passphrase in body
-  if (
-    !req.body["passphrase"] ||
-    !req.body["data"] ||
-    !req.query["game"] ||
-    !valid_games.includes(req.query["game"])
-  )
-    return res.status(400).send({ error: "client" });
+    // require passphrase in body
+    if (
+      !req.body["passphrase"] ||
+      !req.body["data"] ||
+      !req.query["game"] ||
+      !valid_games.includes(req.query["game"])
+    )
+      return res.status(400).send({ error: "client" });
 
-  db = db || client.db("checkdata");
-  progress = progress || db.collection("progress");
-  list = list || db.collection("list");
+    db = db || client.db("checkdata");
+    progress = progress || db.collection("progress");
+    list = list || db.collection("list");
 
-  if (!listdata) listdata = [];
-  if (!listdata[req.query["game"]])
-    listdata[req.query["game"]] = await list.findOne({ game: req.query["game"] });
+    if (!listdata) listdata = [];
+    if (!listdata[req.query["game"]])
+      listdata[req.query["game"]] = await list.findOne({ game: req.query["game"] });
 
-  let passphrase = sjcl.codec.hex.fromBits(
-    sjcl.hash.sha256.hash(req.body["passphrase"])
-  );
+    let passphrase = sjcl.codec.hex.fromBits(
+      sjcl.hash.sha256.hash(req.body["passphrase"])
+    );
 
-  let doc = await progress.findOne(
-    { passphrase },
-    { project: { [`data.${req.query["game"]}`]: 1 } }
-  );
+    let doc = await progress.findOne(
+      { passphrase },
+      { project: { [`data.${req.query["game"]}`]: 1 } }
+    );
 
-  if (!doc) doc = { passphrase, analytics: {}, data: {} }
+    if (!doc) doc = { passphrase, analytics: {}, data: {} }
 
-  let data = doc.data[req.query["game"]] || {};
-  let updated = data;
+    let data = doc.data[req.query["game"]] || {};
+    let updated = data;
 
-  Object.entries(req.body["data"]).map(([key, entry]) => {
-    if (!listdata[req.query["game"]]["list"][key]) return; // make sure the key exists for this game
-    if (data[key]) { // do we have something stored with this key already?
-      if (entry["datetime"] && data[key]["datetime"]) {
-        if (new Date(entry["datetime"]) > new Date(data[key]["datetime"])) {
-          // user data newer than db, take its value
-          updated[key] = {
-            done: entry["done"],
-            datetime: entry["datetime"]
-          };
-        } else {
-          // db data still relevant otherwise
+    Object.entries(req.body["data"]).map(([key, entry]) => {
+      if (!listdata[req.query["game"]]["list"][key]) return; // make sure the key exists for this game
+      if (data[key]) { // do we have something stored with this key already?
+        if (entry["datetime"] && data[key]["datetime"]) {
+          if (new Date(entry["datetime"]) > new Date(data[key]["datetime"])) {
+            // user data newer than db, take its value
+            updated[key] = {
+              done: entry["done"],
+              datetime: entry["datetime"]
+            };
+          } else {
+            // db data still relevant otherwise
+            updated[key] = {
+              done: data[key]["done"],
+              datetime: data[key]["datetime"]
+            };
+          }
+        } else if (!entry["datetime"] && data[key]["datetime"]) {
+          // user data doesn't have a date (not toggled yet), but the db has data
           updated[key] = {
             done: data[key]["done"],
             datetime: data[key]["datetime"]
           };
         }
-      } else if (!entry["datetime"] && data[key]["datetime"]) {
-        // user data doesn't have a date (not toggled yet), but the db has data
+      } else { // db doesn't know about this key, store it
         updated[key] = {
-          done: data[key]["done"],
-          datetime: data[key]["datetime"]
+          done: entry["done"] || false,
+          datetime: entry["datetime"] || new Date(0)
         };
       }
-    } else { // db doesn't know about this key, store it
-      updated[key] = {
-        done: entry["done"] || false,
-        datetime: entry["datetime"] || new Date(0)
-      };
-    }
-  });
+    });
 
-  res.send(updated);
+    res.send(updated);
 
-  await progress.updateOne(
-    { passphrase },
-    {
-      $set: {
-        passphrase,
-        [`data.${req.query["game"]}`]: updated,
-        "analytics.write_last": new Date()
+    await progress.updateOne(
+      { passphrase },
+      {
+        $set: {
+          passphrase,
+          [`data.${req.query["game"]}`]: updated,
+          "analytics.write_last": new Date()
+        },
+        $inc: { "analytics.write": 1 }
       },
-      $inc: { "analytics.write": 1 }
-    },
-    { upsert: true }
-  );
+      { upsert: true }
+    );
+  }
 };
